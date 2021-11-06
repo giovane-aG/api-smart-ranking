0d49ee5808da64df670c870fc2118bc1ff93242d
Giovane <giovaneaalmeida27@gmail.com>
2021-11-06 12:25:17 -0300

    feat: adiciona funções deletar consultar e consultar pelo email

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Jogador } from './interfaces/jogador.interface';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { v4 as uuidv4 } from 'uuid';
@@ -8,18 +8,18 @@ export class JogadoresService {
   private readonly logger = new Logger(JogadoresService.name);
   private jogadores: Array<Jogador> = [];
 
-  async criarAtualizarJogador (criarJogadorDto: CriarJogadorDto) : Promise<void> {
+  public async criarAtualizarJogador (criarJogadorDto: CriarJogadorDto) : Promise<void> {
 
     const jogadorSalvo = this.jogadores.find(jogador => jogador.email === criarJogadorDto.email);
 
     if (jogadorSalvo) {
       return this.atualizar(jogadorSalvo, criarJogadorDto);
     } else {
-      return await this.criar(criarJogadorDto);
+      return this.criar(criarJogadorDto);
     }
   }
   
-  async criar ({ nome, email, telefoneCelular }: CriarJogadorDto) : Promise<void> {
+  public async criar ({ nome, email, telefoneCelular }: CriarJogadorDto) : Promise<void> {
 
     const jogador: Jogador = {
       _id: uuidv4(),
@@ -33,14 +33,29 @@ export class JogadoresService {
     
     this.jogadores.unshift(jogador);
     this.logger.log(`Cria um jogador ->`, jogador);
-    
   }
 
-  async consultarJogadores() : Promise<Jogador[]> {
-    return await this.jogadores; 
+  public async consultarJogadores() : Promise<Jogador[]> {
+    return this.jogadores; 
   }
 
-  private atualizar(jogadorSalvo: Jogador, criarJogadorDto: CriarJogadorDto) : void {
+  public async consultarJogadorPeloEmail (email: string) : Promise<Jogador> {
+    const jogador = this.jogadores.find(jogador => jogador.email === email);
+
+    if (!jogador) throw new NotFoundException(`Nenhum jogador com o email ${email} foi encontrado`);
+
+    return jogador;
+  }
+
+  public atualizar (jogadorSalvo: Jogador, criarJogadorDto: CriarJogadorDto) : void {
     jogadorSalvo.nome = criarJogadorDto.nome;
+  }
+
+  async deletarJogador (email: string) : Promise<void> {
+    const jogadorSalvo = this.jogadores.find(jogador => jogador.email === email);
+
+    if (jogadorSalvo) {
+      this.jogadores = this.jogadores.filter(jogador => jogador.email !== email);
+    }
   }
 }
