bae948dabdc8daa31633eb778b28570e7f445a41
Giovane <giovaneaalmeida27@gmail.com>
2021-11-06 10:48:08 -0300

    feat: cria funções criarAtualizarJogador e consultarJogadores

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
+import { Jogador } from './interfaces/jogador.interface';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+
+  constructor (private readonly jogadoresService: JogadoresService) {}
+
   @Post()
-  async salvarAtualizarJogador() {
-    return { nome: 'Giovane' };
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { nome, email, telefoneCelular } = criarJogadorDto;
+    return await this.jogadoresService.criarAtualizarJogador({ nome, email, telefoneCelular});
+  }
+
+  @Get()
+  async consultarJogadores () : Promise<Jogador[]> {
+    return await this.jogadoresService.consultarJogadores();
   }
 }
