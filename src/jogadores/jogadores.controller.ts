1d9c8b6afc91559ce7f82bf2152d077120146cd1
Giovane <giovaneaalmeida27@gmail.com>
2021-11-06 12:26:25 -0300

    feat: adiciona função deletarJogador

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface';
@@ -11,11 +11,22 @@ export class JogadoresController {
   @Post()
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     const { nome, email, telefoneCelular } = criarJogadorDto;
-    return await this.jogadoresService.criarAtualizarJogador({ nome, email, telefoneCelular});
+    return this.jogadoresService.criarAtualizarJogador({ nome, email, telefoneCelular});
   }
 
   @Get()
-  async consultarJogadores () : Promise<Jogador[]> {
-    return await this.jogadoresService.consultarJogadores();
+  async consultarJogadores (
+    @Query('email') email: string) : Promise<Jogador[] | Jogador | undefined> {
+
+      if (email) {
+        return this.jogadoresService.consultarJogadorPeloEmail(email);
+      } else {
+        return this.jogadoresService.consultarJogadores();
+      }
+  }
+
+  @Delete()
+  async deletarJogador (@Query('email') email: string) : Promise<void> {
+    this.jogadoresService.deletarJogador(email);
   }
 }
