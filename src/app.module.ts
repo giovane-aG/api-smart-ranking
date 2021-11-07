8347ccaa676d11a90739a3c90f7ade2372d1c152
Giovane <giovaneaalmeida27@gmail.com>
2021-11-07 20:46:00 -0300

    feat: injeta MongooseModule nos imports app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://giovane:giovane27@cluster0.mpxxd.mongodb.net/apismartranking?retryWrites=true&w=majority&authSource=admin&readPreference=primary`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    ),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
