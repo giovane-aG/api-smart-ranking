a60d33e9da70be3243244bd56040a497c503df68
Giovane <giovaneaalmeida27@gmail.com>
2021-11-07 21:08:07 -0300

    feat: adiciona pipes para validação dos campos do formulário

--- src/jogadores/dtos/criar-jogador.dto.ts
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsPhoneNumber, IsNotEmpty } from 'class-validator';
+
 export class CriarJogadorDto {
+
+  @IsPhoneNumber()
   readonly telefoneCelular: string;
+  
+  @IsEmail()
   readonly email: string;
+  
+  @IsNotEmpty()
   readonly nome: string;
 }
