9ae709c4a53bd8fdb6f0a38ba08acb771f88c093
Giovane <giovaneaalmeida27@gmail.com>
2021-11-13 18:00:12 -0300

    feat: cria controller categoria

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Categoria } from './interfaces/categoria.interface';
+import { CriarCategoriaDTO } from './dtos/criar-categoria.dto'; 
+import { CategoriasService } from './categorias.service';
+import { AtualizarCategoriaDTO } from './dtos/atualizar-categoria.dto';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+
+  constructor (private readonly categoriaService: CategoriasService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarCategoria (@Body() criarCategoriaDTO: CriarCategoriaDTO) : Promise<Categoria> {
+    return await this.categoriaService.criarCategoria(criarCategoriaDTO);
+  }
+
+  @Get()
+  async consultarCategorias () : Promise<Array<Categoria>> {
+    return await this.categoriaService.consultarCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultadCategoria (@Param('categoria') categoria: string) : Promise<Categoria> {
+    return await this.categoriaService.consultarCategoria(categoria);
+  }
+
+  @Put('/:categoria')
+  async atualizarCategoria (
+    @Param('categoria') categoria: string,
+    @Body() atualizarCategoriaDTO: AtualizarCategoriaDTO) : Promise<void> {
+      return await this.categoriaService.atualizarCategoria(categoria, atualizarCategoriaDTO);
+    }
+
+}
