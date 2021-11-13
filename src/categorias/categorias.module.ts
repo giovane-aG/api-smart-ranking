b40f19b99a243886f333e8afa39aa0a02187cc96
Giovane <giovaneaalmeida27@gmail.com>
2021-11-13 20:39:55 -0300

    feat: importa JogadoresModule no módulo de categorias

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriasController } from './categorias.controller';
 import { CategoriasService } from './categorias.service';
 import { CategoriaSchema } from './interfaces/categoria.schema';
+import { JogadoresModule } from 'src/jogadores/jogadores.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }]),
+    JogadoresModule
+  ],
   controllers: [CategoriasController],
   providers: [CategoriasService]
 })
