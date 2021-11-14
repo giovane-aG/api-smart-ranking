87efa2bab26f5189f316644a12dc5801f053447c
Giovane <giovaneaalmeida27@gmail.com>
2021-11-14 16:32:45 -0300

    feat: cria filtro para tratamento global de excessões

---
+++ src/common/filters/http-exception.ts
@@ -0,0 +1,33 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from "@nestjs/common";
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter  {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { getRequest, getResponse } = host.switchToHttp();
+
+    const request = getRequest();
+    const response = getResponse();
+
+    let status: number;
+    let message: any;
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.getResponse()
+    } else {
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+      message = '';
+    }
+
+    this.logger.error(`Http Status: ${status} Error Message: ${JSON.stringify(message)}`);
+
+    response.status(status).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message
+    });
+
+  }
+}
