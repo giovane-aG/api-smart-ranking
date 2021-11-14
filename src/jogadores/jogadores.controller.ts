fcdb450461e604ee750cbd1814614218ed1206f8
Giovane <giovaneaalmeida27@gmail.com>
2021-11-14 09:37:12 -0300

    refact: move o diretório pipes para o diretório common

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -3,7 +3,7 @@ import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface';
-import { JogadoresValidacaoParametrosPipe } from './pipes/jogadores-validacao-parametros.pipe';
+import { JogadoresValidacaoParametrosPipe } from '../common/pipes/jogadores-validacao-parametros.pipe';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
