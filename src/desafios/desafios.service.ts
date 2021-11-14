e60d98c2eeee0464bcd8f8bd8766dde19138b5db
Giovane <giovaneaalmeida27@gmail.com>
2021-11-14 17:57:20 -0300

    feat: cria funções de consulta de desafios

--- src/desafios/desafios.service.ts
+++ src/desafios/desafios.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Desafio } from './interfaces/desafios.interface';
 
 @Injectable()
-export class DesafiosService {}
+export class DesafiosService {
+  
+  constructor(
+    @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>
+  ) {}
+
+  async consultarDesafios () : Promise<Array<Desafio>> {
+    return await this.desafioModel.find();
+  }
+
+  async consultarDesafioPeloId (_id: string) : Promise<Desafio> {
+    const desafioEncontrado = await this.desafioModel.findOne({ _id });
+
+    if (!desafioEncontrado) {
+      throw new NotFoundException(`Nenhum desafio com o _id ${_id} foi encontrado`);
+    }
+
+    return desafioEncontrado;
+  }
+}
