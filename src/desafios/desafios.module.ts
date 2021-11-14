af03b91344301edcd5822f8946ed0c78b57a8652
Giovane <giovaneaalmeida27@gmail.com>
2021-11-14 17:56:53 -0300

    feat: registra DesafioSchema para o módulo de desafios

--- src/desafios/desafios.module.ts
+++ src/desafios/desafios.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DesafiosController } from './desafios.controller';
 import { DesafiosService } from './desafios.service';
+import { DesafioSchema } from './interfaces/desafio.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Desafio', schema: DesafioSchema  }])],
   providers: [DesafiosService],
   controllers: [DesafiosController]
 })
