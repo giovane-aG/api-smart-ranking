1ce805a5782a5e2cc0e8107d650c276de48b79e7
Giovane <giovaneaalmeida27@gmail.com>
2021-11-20 10:43:31 -0300

    feat: cria handle atualizarDesafio

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { DesafiosService } from './desafios.service';
+import { AtualizarDesafioDTO } from './dtos/atualizar-desafio.dto';
 import { CriarDesafioDTO } from './dtos/criar-desafio.dto';
 import { Desafio } from './interfaces/desafios.interface';
+import { DesafioStatusValidacaoPipe } from './pipes/DesafioStatusValidacao.pipe';
 
 @Controller('api/v1/desafios')
 export class DesafiosController {
@@ -24,4 +26,11 @@ export class DesafiosController {
     return await this.desafiosService.criarDesafio(criarDesafioDTO);
   }
   
+  @Put('/:_id')
+  @UsePipes(ValidationPipe)
+  async atualiarDesafio (
+    @Param('_id') _id: string,
+    @Body(DesafioStatusValidacaoPipe) atualizarDesafioDTO: AtualizarDesafioDTO) : Promise<void> {
+      return await this.desafiosService.atualizarDesafio(_id, atualizarDesafioDTO);
+  }
 }
