6f93620db197b9fc582715d6e3d56cc1e1932546
Giovane <giovaneaalmeida27@gmail.com>
2021-11-20 10:41:58 -0300

    feat: cria DesafioStatusValidacaoPipe

---
+++ src/desafios/pipes/DesafioStatusValidacao.pipe.ts
@@ -0,0 +1,24 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { DesafioStatus } from "../interfaces/desafio-status.enum";
+
+export class DesafioStatusValidacaoPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    const status = value.status
+    
+    if(!this.statusValido(status)) {
+      throw new BadRequestException(`O status ${status} não é válido`);
+    }
+
+    return value;
+  }
+
+  private statusValido(status) {
+    const statusValidos = [
+      DesafioStatus.ACEITO,
+      DesafioStatus.CANCELADO,
+      DesafioStatus.NEGADO,
+    ];
+
+    return statusValidos.includes(status);
+  }
+}
