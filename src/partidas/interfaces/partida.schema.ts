f73d86e0c5b0e550bde900d906e9c5d4806a60ca
Giovane <giovaneaalmeida27@gmail.com>
2021-11-24 10:06:27 -0300

    feat: cria partida schema

---
+++ src/partidas/interfaces/partida.schema.ts
@@ -0,0 +1,18 @@
+import { Schema } from 'mongoose';
+
+export const PartidaSchema = new Schema({
+  categoria: { type: String },
+  jogadores: [{
+    type: Schema.Types.ObjectId,
+    ref : "Jogador"
+  }],
+  def: {
+    type: Schema.Types.ObjectId,
+    ref: "Jogador"
+  },
+  resultado: [{
+    set: {
+      type: String
+    }
+  }]
+}, { timestamps: true, collection: 'partidas' });
