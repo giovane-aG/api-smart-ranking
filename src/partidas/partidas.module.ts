b5b26a0888dcab5182c1d1643bbd9438b1dcee84
Giovane <giovaneaalmeida27@gmail.com>
2021-11-27 14:55:51 -0300

    chore: adiciona imports

--- src/partidas/partidas.module.ts
+++ src/partidas/partidas.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PartidasService } from './partidas.service';
 import { PartidasController } from './partidas.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PartidaSchema } from './interfaces/partida.schema';
+import { JogadoresModule } from 'src/jogadores/jogadores.module';
+import { CategoriasModule } from 'src/categorias/categorias.module';
+import { DesafiosModule } from 'src/desafios/desafios.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Partida', schema: PartidaSchema }]),
+    JogadoresModule,
+    CategoriasModule,
+    DesafiosModule
+  ],
   providers: [PartidasService],
   controllers: [PartidasController]
 })
