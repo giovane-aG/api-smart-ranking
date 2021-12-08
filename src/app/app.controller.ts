5c4c90daa7535194869580affbee2c573cf90c17
Giovane <giovaneaalmeida27@gmail.com>
2021-12-07 21:55:09 -0300

    feat: cria app controller

---
+++ src/app/app.controller.ts
@@ -0,0 +1,12 @@
+import { Bind, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller()
+export class AppController {
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  @Bind(Request())
+  async login(request) {
+    return request.user;
+  }
+}
