014d70f53feac5d48f78d3c13aaffe639b561127
Giovane <giovaneaalmeida27@gmail.com>
2021-12-12 09:27:06 -0300

    chore: adiciona AuthController em AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [UsuariosModule, PassportModule]
+  imports: [UsuariosModule, PassportModule],
+  controllers: [AuthController]
 })
 export class AuthModule {}
