1cfdd168d24c4cc74d1595fb223de1cf8dc900ef
Giovane <giovaneaalmeida27@gmail.com>
2021-12-17 21:31:03 -0300

    feat: injeta jwtService e cria rota função de login

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,18 +2,19 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Usuario } from 'src/usuarios/interfaces/usuario.interface';
-
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  private readonly authService: AuthService;
+  private readonly authService : AuthService;
+  private readonly jwtService  : JwtService;
 
-  constructor(authService: AuthService) {
+  constructor(authService: AuthService, jwtService: JwtService) {
     super({
       usernameField: 'email',
       passwordField: 'senha'
     });
     this.authService = authService;
+    this.jwtService = jwtService;
   }
 
   async validate (email: string, senha: string) : Promise<any> {
@@ -26,5 +27,11 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     return usuario;
   }
 
-
+  async login (user: any) {
+    const payload = { email: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload)
+    };
+  }
+  
 }
