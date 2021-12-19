3c9f05f9298c45e3231b5e4813efd27751c38956
Giovane <giovaneaalmeida27@gmail.com>
2021-12-19 11:48:43 -0300

    refact: remove função login

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,19 +2,16 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   private readonly authService : AuthService;
-  private readonly jwtService  : JwtService;
 
-  constructor(authService: AuthService, jwtService: JwtService) {
+  constructor(authService: AuthService) {
     super({
       usernameField: 'email',
       passwordField: 'senha'
     });
     this.authService = authService;
-    this.jwtService = jwtService;
   }
 
   async validate (email: string, senha: string) : Promise<any> {
@@ -26,12 +23,4 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
     return usuario;
   }
-
-  async login (user: any) {
-    const payload = { email: user.email, sub: user._id };
-    return {
-      access_token: this.jwtService.sign(payload)
-    };
-  }
-  
 }
