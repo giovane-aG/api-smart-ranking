d921420dc7382414becec078370cddeed307213f
Giovane <giovaneaalmeida27@gmail.com>
2021-12-19 11:36:04 -0300

    refact: move função login para auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
   constructor (
-    private readonly usuariosService: UsuariosService
+    private readonly usuariosService: UsuariosService,
+    private readonly jwtService: JwtService
   ) {}
-  
+
   async autenticarUsuario (email: string, inputSenha: string) : Promise<any> {
     const usuario = await this.usuariosService.buscarPeloEmail(email);
 
     if (!usuario) {
       return null;
     }
-    
+
     const senhaValida = inputSenha == usuario.senha;
-    
+
     if (!senhaValida) {
       return null;
     }
 
-    const { senha, ...resto } = usuario;
-    return resto;
-  } 
+    return {
+      email: usuario.email
+    };
+  }
+
+  async login (user: any) {
+    const payload = { email: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload)
+    };
+  }
+
 }
