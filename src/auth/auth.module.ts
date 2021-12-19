6e929aae7e2d2d477867ec08af1e752a70946ad3
Giovane <giovaneaalmeida27@gmail.com>
2021-12-19 16:39:24 -0300

    feat: cria JwtAuthGuard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
-import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Module({
   imports: [
@@ -16,7 +18,8 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '60s' }
     })
   ],
-  providers: [AuthService, LocalStrategy],
-  controllers: [AuthController]
+  exports: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: []
 })
 export class AuthModule {}
